a7dda54def7589dc6114e690b348ce8ca472c5f4
StandeMan <s.freese@st.hanze.nl>
2023-11-10 10:41:25 +0100

    Kaart

--- streamlit.py
+++ streamlit.py
@@ -6,12 +6,66 @@ import seaborn as sns
 from pyproj import Transformer
 import plotly.graph_objects as go
 import streamlit as st
+import requests
+from datetime import date, timedelta
 
 api_data = pd.read_csv('apidata_2021.csv')
 api_data = api_data.drop(columns=['Unnamed: 0'])
 api_data['datetime'] = pd.to_datetime(api_data['datetime'])
 api_data = api_data.groupby([api_data['datetime'].dt.date, api_data['station_id']])[['Windsnelheid (km/h)', 'Windrichting (Graden)', 'Luchtdruk (ps)', 'Zichtbaarheid (Decimeter ver kunnen kijken)', 'Regenval (mm/h)', 'Temperatuur (C)',	'Weercode']].mean().reset_index()
 api_data['station_id'] = api_data['station_id'].astype(str)
+api_data['station_id'] = '0'+api_data['station_id']
+locations = ['06260','06240','06225','06209','06257','06248','06249','06258','06267','06235','06242']
+
+token = "eyJvcmciOiI1ZTU1NGUxOTI3NGE5NjAwMDEyYTNlYjEiLCJpZCI6IjcxZGFmYmI1NGZkYTQ4NjI5ZGU0Mjk5OTM1ZDlmMzdmIiwiaCI6Im11cm11cjEyOCJ9"
+headers = {"Authorization": token}
+
+request = requests.get(url='https://api.dataplatform.knmi.nl/edr/collections/observations/instances/unvalidated/locations', headers=headers)
+jsonre = request.json()   
+
+df = pd.DataFrame(columns=['Naam', 'id'])
+length = len(jsonre['features'])
+
+count = 0 
+while count != length:
+    df.loc[len(df)] = [jsonre['features'][count]['properties']['name'], jsonre['features'][count]['id']]
+    count += 1
+    
+df['Naam'] = df['Naam'].str.lower()
+
+def month(month):
+    weerstat = pd.read_csv('weerstations_noordholland.csv', sep=';')
+    weerstat['station_id'] = '0'
+    weerstat['date'] = date(year=2021, month=month, day=1)
+    weerstat['NAME'] = weerstat['NAME'].str.lower()
+    for i in weerstat.index:
+        for j in df.index:
+            if weerstat.loc[i, 'NAME'] in df.loc[j, 'Naam']:
+                weerstat.loc[i, 'station_id'] = df.loc[j, 'id']
+            if weerstat['NAME'][i] == 'gilze-rijen':
+                weerstat.loc[i, 'station_id'] = '06350'
+            if weerstat['NAME'][i] == 'cabauw mast':
+                weerstat.loc[i, 'station_id'] = '06348'
+            if weerstat['NAME'][i] == 'valkenburg zh':
+                weerstat.loc[i, 'station_id'] = '06210'
+            if weerstat['NAME'][i] == 'hoorn terschelling':
+                weerstat.loc[i, 'station_id'] = '06251'
+            if weerstat['NAME'][i] == 'tholen':
+                weerstat.loc[i, 'station_id'] = '06310'
+            if weerstat['NAME'][i] == 'hoofdplaat':
+                weerstat.loc[i, 'station_id'] = '06310'
+            if weerstat['NAME'][i] == 'schaar':
+                weerstat.loc[i, 'station_id'] = '06310'
+
+    weerstat = weerstat[weerstat['station_id'].isin(locations)]
+    weerstat = weerstat.reset_index(drop=True) 
+    return weerstat.copy()
+
+
+weather_stats = month(1)
+
+map_data = api_data.merge(weather_stats, left_on='station_id', right_on='station_id')
+map_data = map_data.drop(columns=['fid', 'ALT(m)', 'STN_x', 'STN_y', 'date'])
 
 
 tab1, tab2, tab3 = st.tabs(['Gegevens KNMI Weer', 'Gegevens ongelukken', 'Vegelijking'])
@@ -20,12 +74,12 @@ with tab1:
     st.title('Gegevens KNMI weer')
 
     st.subheader('Kaart met weerstations per datum')
-    
+
 
     st.subheader('Lijndiagram')
     Weermeting = st.selectbox('Selecteer weermeting:', key='Weermeting', options=('Windsnelheid (km/h)', 'Windrichting (Graden)', 'Luchtdruk (ps)', 'Zichtbaarheid (Decimeter ver kunnen kijken)', 'Regenval (mm/h)',	'Temperatuur (C)', 'Weercode'))
-    Station = st.selectbox('Selecteer station:', key='Station', options=('6209', '6225', '6235', '6240', '6242', '6248'
-                                                                , '6249', '6257','6258', '6260', '6267'))
+    Station = st.selectbox('Selecteer station:', key='Station', options=('06209', '06225', '06235', '06240', '06242', '06248'
+                                                                , '06249', '06257','06258', '06260', '06267'))
 
     station_filter = api_data[api_data['station_id'] == Station]
     titel = Weermeting+' voor station '+Station
@@ -82,6 +136,12 @@ with tab1:
             via de 'Zonder nul' knop. Door deze visualisatie is goed te zien wat normaal is voor dat gegeven en weerstation.
     """)
 
+    fig4 = plt.figure()
+    map_data = gpd.GeoDataFrame(map_data, geometry=gpd.GeoSeries.from_wkt(map_data['WKT']))
+    first = api_data[api_data['datetime'] == date(2021, 2, 1)]
+    ax = first.plot(linewidth=0.5,column='Windsnelheid (km/h)', legend=True, legend_kwds={"label": "Windsnelheid (km/h)"})
+
+    st.pyplot(fig4)
 
 
 with tab2:
